aff1ef2c173e57a4c34770e2df2119bf13bd0514
Paweł Idziak <pa.idziak@gmail.com>
2018-03-12 13:08:01 +0100

    Fixed hover and active styles for list elements

--- medical-history-webapp/src/app/app.module.ts
+++ medical-history-webapp/src/app/app.module.ts
@@ -12,7 +12,7 @@ import {AuthService} from './_services/auth.service';
 import {CoreComponentsModule} from './core-components/core-components.module';
 import {routing} from './app.routing';
 import {AuthGuard} from './_guard/auth.guard';
-import {AngularFirestore, AngularFirestoreModule} from 'angularfire2/firestore';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {IncidentService} from './_services/incident.service';
 
 
--- medical-history-webapp/src/app/app.routing.ts
+++ medical-history-webapp/src/app/app.routing.ts
@@ -3,11 +3,15 @@ import {ModuleWithProviders} from '@angular/core';
 import {WelcomeComponent} from './core-components/welcome/welcome.component';
 import {MainComponent} from './core-components/main/main.component';
 import {AuthGuard} from './_guard/auth.guard';
+import {TimelineComponent} from "./shared-components/timeline/timeline.component";
 
 export const appRoutes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'main', component: MainComponent, canActivate: [AuthGuard]},
-  {path: '**', redirectTo: 'main'}
+  {path: 'main', component: MainComponent, canActivate: [AuthGuard], children: [
+    {path: ':key', component: TimelineComponent},
+  ]},
+
+  {path: '**', redirectTo: 'main/:key'}
 ]as Routes;
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
